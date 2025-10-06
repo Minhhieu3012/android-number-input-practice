c9efc8dd460b7d4269df38ac6a9041e835dfdf9c
Minhhieu3012 <hieupm9898@ut.edu.vn>
2025-10-06 13:38:06 +0700

    feat: complete number input and horizontal layout

--- app/src/main/java/vn/edu/ut/hieupm9898/numberinputpractice/MainActivity.kt
+++ app/src/main/java/vn/edu/ut/hieupm9898/numberinputpractice/MainActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,7 +32,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import vn.edu.ut.hieupm9898.numberinputpractice.ui.theme.NumberInputPracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -58,5 +76,40 @@ fun NumberInputApp() {
             style = MaterialTheme.typography.titleMedium,
             modifier = Modifier.padding(bottom = 5.dp)
         )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+
+            val textFieldsHeight = 50.dp
+
+            // Tạo ô nhập số lượng
+            TextField(
+                value = inputText,
+                onValueChange = { inputText = it },
+                placeholder = { Text(text = "Nhập vào số lượng") },
+                keyboardOptions = KeyboardOptions (
+                    keyboardType = KeyboardType.Number
+                ),
+                singleLine = true,
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.White,
+                    unfocusedContainerColor = Color.White,
+                    disabledContainerColor = Color.White,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent
+                ),
+                modifier = Modifier
+                    .weight(1f)
+                    .defaultMinSize(minHeight = textFieldsHeight) // Giữ cho chiều cao cố định
+                    .clip(RoundedCornerShape(18.dp))
+                    .border( width = 1.dp, color = Color.Gray,  shape = RoundedCornerShape(18.dp))
+            )
+        }
     }
 }
